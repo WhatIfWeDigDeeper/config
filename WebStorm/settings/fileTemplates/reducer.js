92d6b38cc1a20a7861ad9ba64de2c469254b7216
What If We Dig Deeper <whatifwedigdeeper@gmail.com>
2017-08-12 16:08:00 -0500

    Create reducer.js

---
+++ WebStorm/settings/fileTemplates/reducer.js
@@ -0,0 +1,15 @@
+import * as types from './actionTypes';
+
+const initialState = {
+    
+};
+
+const $ReducerName = (state = initialState, action) => {
+    switch (action.type) {
+        
+        default:
+            return state;
+    }
+};
+
+export default $ReducerName;
