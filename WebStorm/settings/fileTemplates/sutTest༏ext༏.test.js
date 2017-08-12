83e34ab3ef77da9ec50f8f8490456441c73804cf
What If We Dig Deeper <whatifwedigdeeper@gmail.com>
2017-08-12 16:14:57 -0500

    Create sutTest༏ext༏.test.js

---
+++ WebStorm/settings/fileTemplates/sutTest༏ext༏.test.js
@@ -0,0 +1,14 @@
+import * as sut from './${NAME}';
+
+describe('${NAME}', () => {
+
+  describe('', () => {
+
+    it('should ', () => {
+      expect(sut. )
+        .toEqual();
+    });
+
+  });
+
+});
